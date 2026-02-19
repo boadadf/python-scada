d262c3f649ed9f8ff390a5866c46ce1fd9001c73
FERNANDEZ BOADA Daniel <daniel.boada@soprasteria.com>
2026-02-19 20:45:44 +0100

    Fix routing

--- src/openscada_lite/web/config_editor/routes.py
+++ src/openscada_lite/web/config_editor/routes.py
@@ -27,8 +27,8 @@ logger = logging.getLogger(__name__)
 
 SYSTEM_CONFIG_FILENAME = "system_config.json"
 
-CONFIG_FILE = os.path.join(
-    os.path.dirname(__file__), "..", "..", "..", "..", "config", SYSTEM_CONFIG_FILENAME
+CONFIG_FILE = os.environ.get(
+    "SCADA_CONFIG_PATH"
 )
 
 config_router = APIRouter(prefix="/config-editor", tags=["ConfigEditor"])
@@ -51,9 +51,10 @@ def normalize_config_filename(name: str) -> str:
     operation_id="getConfigByName",
 )
 async def get_config_by_name(name: str):
-    config_dir = os.path.dirname(CONFIG_FILE)
     filename = normalize_config_filename(name)
-    path = os.path.join(config_dir, filename)
+    print(f"Getting config for name: {name}, normalized filename: {filename}")
+    path = os.path.join(CONFIG_FILE, filename)
+    print(f"Full path to config file: {path}")
     if not os.path.isfile(path):
         return JSONResponse({"error": "File not found"}, status_code=404)
     async with await anyio.open_file(path, "r") as file:
@@ -68,14 +69,14 @@ async def get_config_by_name(name: str):
     operation_id="getConfigs",
 )
 async def get_configs():
-    logger.debug("Getting configs...")
+    print("Getting configs...")
     config_dir = os.path.dirname(CONFIG_FILE)
-    logger.debug("Config dir:", config_dir)
+    print("Config dir:", config_dir)
     files = [f for f in os.listdir(config_dir) if f.endswith(f"_{SYSTEM_CONFIG_FILENAME}")]
-    logger.debug("Found config files:", files)
+    print("Found config files:", files)
     # Strip suffix for user display
     display_names = [f.replace(f"_{SYSTEM_CONFIG_FILENAME}", "") for f in files]
-    logger.debug("Display names:", display_names)
+    print("Display names:", display_names)
     return display_names
 
 
